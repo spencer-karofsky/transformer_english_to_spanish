8ba68b1afd91dd5946df4bf7099062b07f0747c8
Spencer Karofsky <46546340+spencer-karofsky@users.noreply.github.com>
2025-06-15 11:40:24 -0400

    brief training

--- data/data.py
+++ data/data.py
@@ -9,19 +9,15 @@ batch = next(iter(train_loader))
 print('X shape:', batch['input_ids'].shape)
 print('y shape:', batch['labels'].shape)`
 """
-
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
-dataset = load_dataset('Helsinki-NLP/opus_books', 'en-es')
-
-tokenizer = AutoTokenizer.from_pretrained('Helsinki-NLP/opus-mt-en-es')
-
 class EnglishToSpanish(Dataset):
     def __init__(self, split, max_length=128):
-        self.data = dataset[split]
+        self.data = load_dataset('Helsinki-NLP/opus_books', 'en-es')[split]
+        self.tokenizer = AutoTokenizer.from_pretrained('Helsinki-NLP/opus-mt-en-es')
         self.max_length = max_length
 
     def __len__(self):
@@ -32,9 +28,26 @@ class EnglishToSpanish(Dataset):
         source = sample['translation']['en']
         target = sample['translation']['es']
 
-        source_enc = tokenizer(source, padding='max_length', truncation=True, max_length=self.max_length, return_tensors='pt')
-        target_enc = tokenizer(target, padding='max_length', truncation=True, max_length=self.max_length, return_tensors='pt')
+        source_enc = self.tokenizer(
+            source,
+            padding='max_length',
+            truncation=True,
+            max_length=self.max_length,
+            return_tensors='pt',
+            add_special_tokens=True
+        )
 
-        return {'input_ids': source_enc['input_ids'].squeeze(0),
-                'attention_mask': source_enc['attention_mask'].squeeze(0),
-                'labels': target_enc['input_ids'].squeeze(0)}
+        target_enc = self.tokenizer(
+            target,
+            padding='max_length',
+            truncation=True,
+            max_length=self.max_length,
+            return_tensors='pt',
+            add_special_tokens=True
+        )
+
+        return {
+            'input_ids': source_enc['input_ids'].squeeze(0),
+            'attention_mask': source_enc['attention_mask'].squeeze(0),
+            'labels': target_enc['input_ids'].squeeze(0)
+        }
